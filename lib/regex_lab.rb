7232882d7aa4a0507f2dbedd94ba0722f86c9199
Joshua Wu <jww2145@columbia.edu>
2022-07-25 10:34:23 -0400

    commit tree 8436289fded2168bd57cd09330179ae450d8564e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+    word.match?(/\A[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\A+un|ing+\z/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
